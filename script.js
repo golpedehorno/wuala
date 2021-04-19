8711dcbe26564e124b4be7603d706ae2904ab44e
Natalia Y <natute9@hotmail.com>
2021-04-19 01:05:19 -0300

    4

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+//Variables que pueden cambiar de valor en el tiempo
+var envioGratis = 5600;
+var correo = "Argentino";
 
-
+//Variables que inserta el usuario/cliente
 var nombre = prompt("Hola, podría indicarme su nombre?");
-document.getElementById("titulo").innerHTML= "Bienvenido " + nombre + "!<br> Estás trabajando en: ";
-
-var logo = document.getElementById("logo")
 var logoElegido = prompt ("Indique 1 si trabaja en Wuala o 2 si trabaja en Flexxus")
 
+//INSERTAR TEXTO EN EL HTML 
+document.getElementById("titulo").innerHTML= "Bienvenido " + nombre + "!<br> Estás trabajando en: ";
+
+//INSERTAR IMAGEN EN EL HTML
+var logo = document.getElementById("logo")
 if(logoElegido==1){
     logo.src = "logowuala.png"
 }
@@ -17,16 +22,16 @@ else{
     document.getElementById("titulo").innerHTML=  "<br>";
 }
 
-
+//CAMBIAR DE COLOR TEXTO ANTE EVENTO EN EL DOM
 function cambiarColor(){
       titulo.style.color = "pink" ; 
 }
 
 
-//ENVIO GRATIS - cambiar valor en variable envioGratis y de var correo para que repercuta en el DOM
-var envioGratis = 5600;
+//ENVIO GRATIS - para cambiar valor de la variable envioGratis y de variable correo ir al principio de este documento.
+
 console.log(envioGratis)
 document.getElementById("tarifa1").innerHTML = "en compras superiores de $" + "<b>" + envioGratis +"</b>";
 
-var correo = "Argentino";
+
 document.getElementById("tarifa2").innerHTML = "<b>" + correo +"</b>";
